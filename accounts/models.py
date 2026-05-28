183c4a2378ac0684e663e3736c037c92dcacd2db
Sid <sahani.bunty9@gmail.com>
2026-05-28 12:09:08 +0530

    Added Potgresql in place of SQLITE3

--- accounts/models.py
+++ accounts/models.py
@@ -6,21 +6,36 @@ from django.dispatch import receiver
 
 # Create your models here.
 class Profile(models.Model):
+
     ROLE_CHOICES = (
         ("candidate", "Candidate"),
         ("recruiter", "Recruiter"),
     )
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="candidate")
+
     resume = models.FileField(upload_to="resumes/", null=True, blank=True)
+
     bio = models.TextField(null=True, blank=True)
 
     def __str__(self):
+
         return self.user.username
 
 
 @receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
+def create_user_profile(sender, instance, created, raw, **kwargs):
+    """
+    raw=True during loaddata
+
+    Skip profile creation while importing fixtures
+    """
+
+    if raw:
+
+        return
 
     if created:
 
--- job_portal/settings.py
+++ job_portal/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = "job_portal.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "job_portal_db",
+        "USER": "postgres",
+        "PASSWORD": "PomBunty@123",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
